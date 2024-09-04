79433d45fd96bfa458c0c93fc99b6896a086ba2d
edycyber <foodworldmeta@gmail.com>
2024-09-04 17:10:31 +0200

    Create validate.js

---
+++ validate.js
@@ -0,0 +1,11 @@
+function validate() {
+    const code = document.getElementById('codeInput').value.trim();
+    
+    const validCodes = ['AI1532', 'AI1234', 'AI9876'];
+
+    if (validCodes.includes(code)) {
+        window.location.href = 'create.html';
+    } else {
+        window.location.href = 'https://foodiverse0.gumroad.com/l/ppu';
+    }
+}
