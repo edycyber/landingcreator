b7b2857fc59898530af54e5b615f7e229726ef4f
edycyber <foodworldmeta@gmail.com>
2024-09-07 14:39:38 +0000

    wow04

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 import google.generativeai as genai
 
 # Configure the API key directly in the script
-API_KEY = 'AIzaSyA5Id4uuQuZLDdpfZZeMwa1ZhtSAaItUdI'
+API_KEY = 'API_KEY'
 genai.configure(api_key=API_KEY)
 
 # Generation configuration
