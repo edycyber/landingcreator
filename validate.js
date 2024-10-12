4bfa9badf5642041a7efc929d4d950423723a111
edycyber <foodworldmeta@gmail.com>
2024-10-12 17:08:12 +0200

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,7 +1,7 @@
 function validate() {
     const code = document.getElementById('codeInput').value.trim();
     
-    const validCodes = ['AI1532', 'AI1234', 'AI9876'];
+    const validCodes = ['AI1532', 'AI1234', 'AI9876','BP-4937'];
 
     if (validCodes.includes(code)) {
         window.location.href = 'choose.html';
