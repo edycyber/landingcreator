9fdab45a7095fc860b69ffaa8673283f8f89cfe2
edycyber <foodworldmeta@gmail.com>
2024-10-12 17:23:12 +0200

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,11 +1,11 @@
 function validate() {
     const code = document.getElementById('codeInput').value.trim();
     
-    const validCodes = ['AI1532', 'AI1234', 'AI9876','BP-4937'];
+    const validCodes = ['AI1532', 'AI1234', 'AI9876','BP-4937','PP-59874'];
 
     if (validCodes.includes(code)) {
         window.location.href = 'choose.html';
     } else {
-        window.location.href = 'https://foodiverse0.gumroad.com/l/ppu';
+        window.location.href = 'https://foodiverse0.gumroad.com/l/premiumplan';
     }
 }
